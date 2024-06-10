20b4c353cfaab0fbd061a205f201a0f0fd98eb9a
Frankosviz <geom.francescopaolo@gmail.com>
2024-06-10 16:38:31 +0200

    Milestone 1 and 2 : Done

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,20 +11,29 @@ class ProjectController extends Controller
     public function index()
     // inviamo la risposta tramite file json al nostro front-end
     {
+        // Possiamo passare i dati al front end anche paginati
+        // $projects = Project::paginate(3) | invece di | $projects = Project::all();
         $projects = Project::all();
         return response()->json([
             'success' => true,
             'results' => $projects
         ]);
     }
-
+    // Possiamo passare anche lo slug invece dell'id
     public function show($id)
     {
-        // Andiamo a passare tutti i dati del database relazionati al nostro frontend tramite il ->with()
+        // Andiamo a passare tutti i dati del database relazionati al nostro frontend tramite il ->with()... Eager Loading e Lazy Loading (da studiare)
         $project = Project::where('id', $id)->with('type', 'technologies')->first();
-        return response()->json([
-            'success' => true,
-            'results' => $project
-        ]);
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Project not found'
+            ]);
+        }
     }
 }
