a0c60a416854c52fc4060a5c3170d8b66b5ac19a
Frankosviz <geom.francescopaolo@gmail.com>
2024-06-11 19:32:49 +0200

    Corrected Show and index with 200 and 404

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -15,25 +15,27 @@ class ProjectController extends Controller
         // $projects = Project::paginate(3) | invece di | $projects = Project::all();
         $projects = Project::all();
         return response()->json([
-            'success' => true,
+            'status' => 'success',
+            'message' => 'Ok',
             'results' => $projects
-        ]);
+        ], 200);
     }
     // Possiamo passare anche lo slug invece dell'id
-    public function show($id)
+    public function show($slug)
     {
         // Andiamo a passare tutti i dati del database relazionati al nostro frontend tramite il ->with()... Eager Loading e Lazy Loading (da studiare)
-        $project = Project::where('id', $id)->with('type', 'technologies')->first();
+        $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
         if ($project) {
             return response()->json([
-                'success' => true,
+                'status' => 'success',
+                'message' => 'Ok',
                 'results' => $project
-            ]);
+            ], 200);
         } else {
             return response()->json([
-                'success' => false,
+                'status' => 'error',
                 'message' => 'Project not found'
-            ]);
+            ], 404);
         }
     }
 }
